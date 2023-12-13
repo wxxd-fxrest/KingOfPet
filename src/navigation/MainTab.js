12bd32f2a0b7c7a706ce705af08149dd8c245cf5
fxrest704 <fxrest704@gmail.com>
2023-12-13 16:38:24 +0900

    Alarm: 알림 페이지 대략적인 레이아웃 구성

--- src/navigation/MainTab.js
+++ src/navigation/MainTab.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import styled from 'styled-components';
 import MainScreen from '../screens/main/MainScreen';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import AlertScreen from '../screens/alert/AlertScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const MainTab = ({ handleScroll, hide }) => {
     return (
         <Tab.Navigator
+            initialRouteName="Main"
             screenOptions={{
                 headerShown: false,
                 tabBarShowLabel: false,
@@ -20,6 +23,26 @@ const MainTab = ({ handleScroll, hide }) => {
                 headerStyle: { backgroundColor: '#f9f9f7' },
             }}
         >
+            <Tab.Screen
+                name="Alert"
+                component={AlertScreen}
+                options={{
+                    title: '알림',
+                    headerShown: true,
+                    headerShadowVisible: false,
+                    showLable: false,
+                    tabBarIcon: ({ focused, size }) => {
+                        return (
+                            <MaterialCommunityIcons
+                                name={focused ? 'bell-badge' : 'bell-badge-outline'}
+                                size={28}
+                                color={focused ? '#243e35' : '#c1ccc8'}
+                            />
+                        );
+                    },
+                }}
+            />
+
             <Tab.Screen
                 name="Main"
                 children={() => <MainScreen handleScroll={handleScroll} hide={hide} />}
---
+++ src/screens/alert/AlertScreen.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import styled from 'styled-components';
+import postData from '../../data/postData';
+
+const AlertScreen = () => {
+    return (
+        <Container>
+            <FlatList
+                data={postData}
+                ItemSeparatorComponent={heightEmpty}
+                keyExtractor={(item) => item.id + ''}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <AlertBox>
+                        <AlertUserProfileBox>
+                            <UserImageBox>
+                                <UserImg source={{ uri: item.image }} />
+                            </UserImageBox>
+                            <UserNameTag>
+                                <UserName>{item.username}</UserName>
+                                <AlertDetail>님이 나를 구독했습니다.</AlertDetail>
+                            </UserNameTag>
+                        </AlertUserProfileBox>
+                    </AlertBox>
+                )}
+            />
+        </Container>
+    );
+};
+
+const Container = styled.View`
+    background-color: #f9f9f7;
+    flex: 1;
+    padding: 0px 20px;
+`;
+
+const heightEmpty = styled.View`
+    height: 20px;
+`;
+
+const AlertBox = styled.TouchableOpacity``;
+
+const AlertUserProfileBox = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const UserImageBox = styled.View`
+    background-color: yellow;
+    width: 40px;
+    height: 40px;
+    border-radius: 100px;
+    margin-right: 12px;
+`;
+
+const UserImg = styled.Image`
+    width: 100%;
+    height: 100%;
+    border-radius: 100px;
+`;
+
+const UserNameTag = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const UserName = styled.Text`
+    font-size: 14px;
+    font-weight: 600;
+    color: #343c3a;
+`;
+
+const AlertDetail = styled.Text`
+    font-size: 14px;
+    font-weight: 400;
+    color: #343c3a;
+`;
+
+export default AlertScreen;
